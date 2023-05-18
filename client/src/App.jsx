cae054daa124d2cfba6c64fafa176ed4bf671ba1
SujayCJoshy <sujaycjoshy@gmail.com>
2023-05-18 21:01:47 +0200

    Components and Pages and basic routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import IndexPage from './pages/IndexPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 function App() {
   return (
-  <div className="bg-gray-100">test</div>
+  <Routes>
+    <Route index element={<IndexPage />} />
+    <Route path="/login" element={<LoginPage />} />
+  </Routes>
   )
 }
 
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Header />
+      <h1>Login</h1>
+      <Footer />
+    </div>
+  );
+}
