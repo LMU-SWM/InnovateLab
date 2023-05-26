6dad653f2c5c857b3f654b1300063e7a6ac3d196
LMU-SWM <117455430+Iisaabeel@users.noreply.github.com>
2023-05-26 14:24:26 +0200

    Auth0 Authentication
    Authentication implemented at the domain level

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import LogoutButton from './LogoutButton.jsx';
+
 export default function Header() {
   return (
-    <div class="bg-grey-100">
-      <h1>Header Component</h1>
-    </div>
+    <div>Header</div>
+    /*<main className="column">
+      <LogoutButton/>
+    </main>*/
   );
 }
