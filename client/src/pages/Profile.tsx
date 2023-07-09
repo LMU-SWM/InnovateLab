d726473f56733d7b1eff15c34feaf590f4a5a438
LMU-SWM <132190705+LMU-SWM@users.noreply.github.com>
2023-07-09 12:18:29 +0200

    personalEventsCalendar
    Co-Authored-By: SujayCJoshy <117585316+SujayCJoshy@users.noreply.github.com>
    Co-Authored-By: Iisaabeel <117455430+Iisaabeel@users.noreply.github.com>
    Co-Authored-By: nika-ziab <122452351+nika-ziab@users.noreply.github.com>

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,14 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Grid, Avatar, TextField, Button } from "@mui/material";
+import {
+  Grid,
+  Avatar,
+  TextField,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Checkbox,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import createAuth0Client, { Auth0Client } from "@auth0/auth0-spa-js";
 
-function Profile() {
+function Profile(): JSX.Element {
   const { isAuthenticated, user, getIdTokenClaims } = useAuth0();
 
-  const [calendars, setCalendars] = useState([]);
+  const [calendars, setCalendars] = useState<any[]>([]);
+  const [selectedCalendars, setSelectedCalendars] = useState<any[]>([]);
+  const [events, setEvents] = useState<any[]>([]);
 
-  const fetchCalendars = async (accessToken: String) => {
+  const fetchCalendars = async (accessToken: string) => {
     try {
       const response = await fetch(
         "https://www.googleapis.com/calendar/v3/users/me/calendarList",
@@ -26,6 +41,28 @@ function Profile() {
       }
     } catch (error) {
       console.error("Error fetching calendars:", error);
+      throw error;
+    }
+  };
+
+  const fetchEvents = async (accessToken: string, calendarId: string) => {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        return data.items; // Returns the list of events
+      } else {
+        throw new Error("Failed to fetch events");
+      }
+    } catch (error) {
+      console.error("Error fetching events:", error);
       throw error;
     }
   };
@@ -58,16 +95,17 @@ function Profile() {
           // Parse the response JSON
           const data = await response.json();
           console.log("GOOGLE:", data.googleCalendarAccessToken);
+          localStorage.setItem("GOOGLE_TOKEN", data.googleCalendarAccessToken);
           try {
             const googleCalendars = await fetchCalendars(
               data.googleCalendarAccessToken
             );
             console.log("Google Calendars:", googleCalendars);
+            setCalendars(googleCalendars);
           } catch (e: any) {
             console.error("Error:", e);
           }
           // Log the returned JSON to the console
-          
         } else {
           console.error("Error: ID token claims not available");
         }
@@ -75,6 +113,41 @@ function Profile() {
     };
     getTokenAndSave();
   }, [isAuthenticated, getIdTokenClaims]);
+
+  const handleCalendarSelection = (calendar: any) => {
+    if (selectedCalendars.includes(calendar)) {
+      setSelectedCalendars((prevSelectedCalendars) =>
+        prevSelectedCalendars.filter((c) => c.id !== calendar.id)
+      );
+    } else {
+      setSelectedCalendars((prevSelectedCalendars) => [
+        ...prevSelectedCalendars,
+        calendar,
+      ]);
+    }
+  };
+
+  const handleFormSubmit = async () => {
+    const selectedCalendarEvents: any[] = [];
+    const googleToken : string = localStorage.getItem("GOOGLE_TOKEN")||"";
+    for (const calendar of selectedCalendars) {
+      try {
+        const events = await fetchEvents(
+          googleToken,
+          calendar.id
+        );
+        selectedCalendarEvents.push(...events);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    }
+
+    console.log("Selected Calendars:", selectedCalendars);
+    console.log("Events:", selectedCalendarEvents);
+
+    const eventsJSON = JSON.stringify(selectedCalendarEvents);
+    console.log("Events JSON:", eventsJSON);
+  };
 
   return (
     <main style={{ padding: "1rem 0" }}>
@@ -109,6 +182,47 @@ function Profile() {
           </Grid>
         </Grid>
       )}
+
+      <section style={{ backgroundColor: "#f0f0f0", padding: "1rem" }}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Select the calendars you want us to use
+        </Typography>
+
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Calendar</TableCell>
+                <TableCell align="center">Select</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {calendars.map((calendar) => (
+                <TableRow key={calendar.id}>
+                  <TableCell component="th" scope="row">
+                    {calendar.summary}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Checkbox
+                      checked={selectedCalendars.includes(calendar)}
+                      onChange={() => handleCalendarSelection(calendar)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleFormSubmit}
+          style={{ marginTop: "1rem" }}
+        >
+          Submit
+        </Button>
+      </section>
     </main>
   );
 }
